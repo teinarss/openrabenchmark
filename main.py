4f4a9b1d7daa07ee60bcc4680fe748e7441e8687
teinarss <einarsson.tomas@gmail.com>
2019-06-02 20:22:30 +0200

    aoe

--- main.py
+++ main.py
@@ -7,12 +7,36 @@ import numpy as np
 # import numpy as np
 # import gc
 
-df = pd.read_csv('new.csv').drop_duplicates()
-df1 = pd.read_csv('old.csv').drop_duplicates()
-print(df.size)
-print(df.head(5))
+# combine all runs for a target to one DF
+#df = np.genfromtxt('old.csv', delimiter=',')[:1]
 
+# df = pd.read_csv('cpu_new2.csv')
+# df1 = pd.read_csv('cpu_old1.csv')
+baseline_files = ['C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/baseline-0Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/baseline-1Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/baseline-2Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/baseline-3Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/baseline-4Pathfinder.csv']
+pr_files = ['C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/pr-0Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/pr-1Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/pr-2Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/pr-3Pathfinder.csv', 'C:/Users/Tomas.PYTTEMJUK/Documents/OpenRA/Logs/pr-4Pathfinder.csv']
+
+def readData(files):
+    li = []
+    for filename in files:
+        df = pd.read_csv(filename, index_col=None, header=0, usecols=['time [ms]'])
+        li.append(df)
+
+    return pd.concat(li, axis=0, ignore_index=True)
+
+baseline = readData(baseline_files)
+pr = readData(pr_files)
+# df = pd.read_csv('render_new3.csv', usecols = ['time'])
+
+# df1 = pd.read_csv('render_old1.csv')
+print(baseline.size)
+print(baseline.head(5))
+print(baseline.tail(5))
+
+#baseline = baseline.astype(float)
 # plt.figure(); 
-ax = df.plot(kind='hist', x='tick', y='time', label='new', color='tab:red', figsize=(20,10), stacked=True, alpha=0.5,bins=np.linspace(0.001, 15.001, 50), log=True); 
-df1.plot(kind='hist', x='tick', y='time', label='old',color='tab:blue', ax=ax, stacked=True, alpha=0.5, bins=np.linspace(0.001, 15.001, 50), log=True); 
+
+
+ax = baseline.plot(kind='hist', label='baseline', color='tab:red', figsize=(20,10), stacked=True, alpha=0.5,bins=np.linspace(0.001, 30.001, 50), log=True); 
+pr.plot(kind='hist', label='pr',color='tab:blue', ax=ax, stacked=True, alpha=0.5, bins=np.linspace(0.001, 30.001, 50), log=True); 
+plt.legend();
 plt.show()
